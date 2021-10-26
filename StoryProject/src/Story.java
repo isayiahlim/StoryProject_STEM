f32dbca5a8a09a99ed8fcf12cf29893dfcac5fa3
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-10-26 08:57:52 -0700

    Header complete, most of the methods too

--- StoryProject/src/Story.java
+++ StoryProject/src/Story.java
@@ -5,8 +5,8 @@
  * Story Project
  * 
  * Program Description (in your own words):
- * 
- * 
+ * This code writes a mad lib-style story with trees between each of the paragraphs. The user 
+ * will input words to fill in the story, and then the story will print out.
  */
 //imports scanners
 import java.util.Scanner;
@@ -27,10 +27,62 @@ public class Story {
 	//rotates the letters right by two
 	public static String rotateRight(Scanner input, String question)
 	{
-		System.out.println("Write a sentence:");
+		System.out.println(question);
 		String sentence = input.nextLine();
 		int length = sentence.length();
-		String newSentence = sentence.substring(2,length) + sentence.substring(0,2);
+		String newSentence = sentence.substring(length-2) + sentence.substring(0,length-2);
 		return newSentence; 
 	}
+	
+	//rotates the letters right by the number int num 
+	public static String rotateRight(Scanner input, String question, int num)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		int length = sentence.length();
+		String newSentence = sentence.substring(length-num) + sentence.substring(0,length-num);
+		return newSentence; 
+	}
+	
+	//rotates the letters left by two
+	public static String rotateLeft(Scanner input, String question)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		String newSentence = sentence.substring(2) + sentence.substring(0,2);
+		return newSentence; 
+	}
+	
+	//rotates the letters left by number num
+	public static String rotateLeft(Scanner input, String question, int num)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		String newSentence = sentence.substring(num) + sentence.substring(0,num);
+		return newSentence; 
+	}
+	
+	//skips every other letter in an answer
+	public static String skipLetters(Scanner input, String question)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		String newSentence = "";
+		for(int i = 0; i < sentence.length(); i+=2)
+			newSentence += sentence.substring(i,i+1);
+		return newSentence;
+	}
+	
+	//skips every other letter, beginning at the starting int
+	public static String skipLetters(Scanner input, String question, int start)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		String newSentence = "";
+		for(int i = start; i < sentence.length(); i+=2)
+			newSentence += sentence.substring(i,i+1);
+		return newSentence;
+	}
+	
+	//reverses the input
 }
