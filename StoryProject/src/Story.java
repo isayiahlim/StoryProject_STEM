9302906ce83b6b9a13cc102d14bd38893ccc0096
TeslaSTEMCSTeacher <bkankelborg@lwsd.org>
2021-10-26 08:27:11 -0700

    Populating starter project: Part 2

---
+++ StoryProject/src/Story.java
@@ -0,0 +1,17 @@
+/**
+ * Name: 
+ * Date Last Updated: 
+ * APCSA Period: 
+ * Story Project
+ * 
+ * Program Description (in your own words):
+ * 
+ * 
+ */
+
+public class Story {
+	public static void main(String[] args) {
+		
+	}
+	
+}
