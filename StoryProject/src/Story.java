c81964da49488141e1f08b4d598f05ce497ddd8d
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-10-26 09:25:30 -0700

    one method left!

--- StoryProject/src/Story.java
+++ StoryProject/src/Story.java
@@ -85,4 +85,47 @@ public class Story {
 	}
 	
 	//reverses the input
+	public static String reverse(Scanner input, String question)
+	{
+		System.out.println(question);
+		String sentence = input.nextLine();
+		String newString = "";
+		int length = sentence.length();
+		for (int i = length-1; i > 0; i--)
+			newString += sentence.substring(i,i+1);
+		return newString;
+	}
+	
+	//mixes up three words according to the number input by the user
+	public static String wordMix(Scanner input)
+	{
+		//stores the three words
+		System.out.println("Input three words, separated by spaces. Then hit enter.");
+		String first = input.next();
+		String second = input.next();
+		String third = input.next();
+		System.out.println("Input a number between 1 and 10. Then hit enter.");
+		int num = input.nextInt();
+		
+		//takes the middle of each word
+		String firstMiddle = first.substring(first.length()/2-1,first.length()/2 +1);
+		String secondMiddle = second.substring(second.length()/2-1,second.length()/2 +1);
+		String thirdMiddle = third.substring(third.length()/2-1,third.length()/2 +1);
+		String finalString = "";
+		
+		//repeats the middles number num times
+		for (int i = 0; i < num; i++)
+		{
+			finalString += firstMiddle;
+			finalString += secondMiddle;
+			finalString += thirdMiddle;
+		}
+		return finalString;
+	}
+	
+	/*
+	 * finds the vowels n a string, returns the string starting from the last time a vowel 
+	 * occurs for the first time.
+	*/
+	
 }
