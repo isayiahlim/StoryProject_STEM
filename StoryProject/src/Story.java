8dce131b2f4463f68c2d4f936d5a147765bf194b
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-10-26 08:39:53 -0700

    This is my first commit

--- StoryProject/src/Story.java
+++ StoryProject/src/Story.java
@@ -1,17 +1,36 @@
 /**
- * Name: 
- * Date Last Updated: 
- * APCSA Period: 
+ * Name: Isayiah Lim
+ * Date Last Updated: 10/26/2021
+ * APCSA Period: 2
  * Story Project
  * 
  * Program Description (in your own words):
  * 
  * 
  */
-
+//imports scanners
+import java.util.Scanner;
 public class Story {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 	}
 	
+	//asks for first name
+	public static String nameGame(Scanner input)
+	{
+		System.out.println("What is your first name: ");
+		String name = input.next();
+		return name;
+	}
+	
+	//rotates the letters right by two
+	public static String rotateRight(Scanner input, String question)
+	{
+		System.out.println("Write a sentence:");
+		String sentence = input.nextLine();
+		int length = sentence.length();
+		String newSentence = sentence.substring(2,length) + sentence.substring(0,2);
+		return newSentence; 
+	}
 }
